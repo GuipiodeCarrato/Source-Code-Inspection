6398b94c0c19c011fa829e7a1c148c0d95478f0d
Guilherme Pio de Carrato <44588805+GuipiodeCarrato@users.noreply.github.com>
2020-03-12 13:14:44 -0300

    Arquivos checados
    Arquivos com os erros comentados

---
+++ Troco.java
@@ -0,0 +1,90 @@
+
+//Aluno: Guilherme Pio
+
+
+package br.calebe.ticketmachine.core;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+class Troco {
+
+    protected PapelMoeda[] papeisMoeda;
+
+    public Troco(int valor) { // Comissão: implementado incorretamente
+        papeisMoeda = new PapelMoeda[6]; // Comissão: implementado incorretamente
+        int count = 0;
+        while (valor % 100 != 0) {
+            count++;
+        }
+        papeisMoeda[5] = new PapelMoeda(100, count);
+        count = 0;
+        while (valor % 50 != 0) {
+            count++;
+        }
+        papeisMoeda[4] = new PapelMoeda(50, count);
+        count = 0;
+        while (valor % 20 != 0) {
+            count++;
+        }
+        papeisMoeda[3] = new PapelMoeda(20, count);
+        count = 0;
+        while (valor % 10 != 0) {
+            count++;
+        }
+        papeisMoeda[2] = new PapelMoeda(10, count);
+        count = 0;
+        while (valor % 5 != 0) {
+            count++;
+        }
+        papeisMoeda[1] = new PapelMoeda(5, count);
+        count = 0;
+        while (valor % 2 != 0) {
+            count++;
+        }
+        papeisMoeda[1] = new PapelMoeda(2, count); // Excesso: trecho de código desnecessário
+    }
+
+    public Iterator<PapelMoeda> getIterator() {
+        return new TrocoIterator(this);
+    }
+
+    class TrocoIterator implements Iterator<PapelMoeda> {
+
+        protected Troco troco;
+
+        public TrocoIterator(Troco troco) {
+            this.troco = troco;
+        }
+
+        @Override
+        public boolean hasNext() {
+            for (int i = 6; i >= 0; i++) { // Controle: Desvio condicional incorreto
+                if (troco.papeisMoeda[i] != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public PapelMoeda next() {
+            PapelMoeda ret = null;
+            for (int i = 6; i >= 0 && ret != null; i++) { // Controle: Desvio condicional incorreto
+                if (troco.papeisMoeda[i] != null) {
+                    ret = troco.papeisMoeda[i];
+                    troco.papeisMoeda[i] = null;
+                }
+            }
+            return ret;
+        }
+
+        @Override
+        public void remove() { //Excesso: código desnecessário
+            next();
+        }
+    }
+}
